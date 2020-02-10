97dee6a8187ebeb1dbbb064e10ab0df1405828ad
Alberto Tarantino <albertotarantino75@yahoo.it>
2020-02-10 13:37:44 +0100

    basic input binding

---
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		ProcessInput();
+	}
+
+  private void ProcessInput () {
+    if (Input.GetKey(KeyCode.Space)) {
+      print("Thrusting");
+    }
+    if (Input.GetKey(KeyCode.A)) {
+      print("Rotating Left");
+    }
+    else if (Input.GetKey(KeyCode.D)) {
+      print("Rotating Right");
+    }
+  }
+}
