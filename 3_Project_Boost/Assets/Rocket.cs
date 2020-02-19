94b1b18d53051bee6f69bd5e429d74d5113e0ffb
Alberto Tarantino <albertotarantino75@yahoo.it>
2020-02-19 12:32:25 +0100

    Adding audio

--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
   Rigidbody rigidBody;
+  AudioSource audioSource;
+
 
 	// Use this for initialization
 	void Start () {
     rigidBody = GetComponent<Rigidbody>();
+    audioSource = GetComponent<AudioSource>();
+
 	}
 
 	// Update is called once per frame
@@ -18,7 +22,15 @@ public class Rocket : MonoBehaviour {
 
   private void ProcessInput () {
     if (Input.GetKey(KeyCode.Space)) {
+
       rigidBody.AddRelativeForce(Vector3.up);
+
+      if (!audioSource.isPlaying) {
+        audioSource.Play();
+        }
+      else {
+        audioSource.Stop();
+      }
     }
     if (Input.GetKey(KeyCode.A)) {
       transform.Rotate(Vector3.forward);
