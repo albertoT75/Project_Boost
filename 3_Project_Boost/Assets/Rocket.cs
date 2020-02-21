178cefd927aacfd0fb7ce42f13f3e0c548f5cbef
Alberto Tarantino <albertotarantino75@yahoo.it>
2020-02-21 12:51:49 +0100

    serializing thursts

--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+  [SerializeField] float rcsThrust = 150f;
+  [SerializeField] float mainThrust = 100f;
   Rigidbody rigidBody;
   AudioSource audioSource;
 
@@ -23,9 +25,10 @@ public class Rocket : MonoBehaviour {
 
   private void Thrust()
   {
+
     if (Input.GetKey(KeyCode.Space))
     {
-      rigidBody.AddRelativeForce(Vector3.up);
+      rigidBody.AddRelativeForce(Vector3.up * mainThrust);
       if (!audioSource.isPlaying)
       {
         audioSource.Play();
@@ -41,13 +44,15 @@ public class Rocket : MonoBehaviour {
   private void Rotate()
   {
     rigidBody.freezeRotation = true; // take manual control of rotation
+    float rotationThisFrame = rcsThrust * Time.deltaTime;
+
     if (Input.GetKey(KeyCode.A))
     {
-      transform.Rotate(-Vector3.forward);
+      transform.Rotate(-Vector3.forward * rotationThisFrame);
     }
     else if (Input.GetKey(KeyCode.D))
     {
-      transform.Rotate(Vector3.forward);
+      transform.Rotate(Vector3.forward * rotationThisFrame);
     }
     rigidBody.constraints = RigidbodyConstraints.FreezeRotationX
         | RigidbodyConstraints.FreezeRotationY
