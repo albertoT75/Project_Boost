1007d16e9dbec763fd9ced80a6969af7f8c9e471
Alberto Tarantino <albertotarantino75@yahoo.it>
2020-02-21 12:14:22 +0100

    Resolving Movement bugs

--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -11,32 +11,49 @@ public class Rocket : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
     rigidBody = GetComponent<Rigidbody>();
+    rigidBody.constraints = RigidbodyConstraints.FreezePositionZ & RigidbodyConstraints.FreezeRotationZ;
     audioSource = GetComponent<AudioSource>();
-
 	}
 
 	// Update is called once per frame
-	void Update () {
-		ProcessInput();
+	void Update() {
+    Thrust();
+		Rotate();
 	}
 
-  private void ProcessInput () {
-    if (Input.GetKey(KeyCode.Space)) {
-
+  private void Thrust()
+  {
+    if (Input.GetKey(KeyCode.Space))
+    {
       rigidBody.AddRelativeForce(Vector3.up);
-
-      if (!audioSource.isPlaying) {
+      if (!audioSource.isPlaying)
+      {
         audioSource.Play();
-        }
-      else {
-        audioSource.Stop();
       }
     }
-    if (Input.GetKey(KeyCode.A)) {
-      transform.Rotate(Vector3.forward);
-    }
-    else if (Input.GetKey(KeyCode.D)) {
-      transform.Rotate(-Vector3.forward);
+    else
+    {
+        audioSource.Stop();
     }
   }
+
+
+  private void Rotate()
+  {
+    rigidBody.freezeRotation = true; // take manual control of rotation
+    if (Input.GetKey(KeyCode.A))
+    {
+      transform.Rotate(-Vector3.forward);
+    }
+    else if (Input.GetKey(KeyCode.D))
+    {
+      transform.Rotate(Vector3.forward);
+    }
+    rigidBody.constraints = RigidbodyConstraints.FreezeRotationX
+        | RigidbodyConstraints.FreezeRotationY
+        | RigidbodyConstraints.FreezePositionZ; // resume physics control of rotation
+  }
+
 }
+
+
