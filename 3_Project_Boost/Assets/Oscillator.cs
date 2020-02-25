4c03c538c149f26f52af566675680192ae0c8f1f
Alberto Tarantino <albertotarantino75@yahoo.it>
2020-02-25 15:26:13 +0100

    setting up moving platform

---
+++ 3_Project_Boost/Assets/Oscillator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+
+  [SerializeField] Vector3 movementVector;
+
+  // todo remove from inpector later
+  [Range(0,1)][SerializeField]float movementFactor; // o not moved, 1 fully moved
+
+  Vector3 startingPos;
+
+	// Use this for initialization
+	void Start ()
+  {
+    startingPos = transform.position; // where the object is
+	}
+
+	// Update is called once per frame
+	void Update ()
+  {
+    Vector3 offset = movementVector * movementFactor;
+    transform.position = startingPos + offset;
+	}
+}
