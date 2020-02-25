ed1714748d6db5104827eb3ec34269f5b052e47e
Alberto Tarantino <albertotarantino75@yahoo.it>
2020-02-25 15:46:20 +0100

    adding movement to platform

--- 3_Project_Boost/Assets/Oscillator.cs
+++ 3_Project_Boost/Assets/Oscillator.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour {
 
-  [SerializeField] Vector3 movementVector;
+  [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+  [SerializeField] float period = 2f;
 
   // todo remove from inpector later
-  [Range(0,1)][SerializeField]float movementFactor; // o not moved, 1 fully moved
-
+  float movementFactor; // o not moved, 1 fully moved
   Vector3 startingPos;
 
 	// Use this for initialization
@@ -22,6 +22,13 @@ public class Oscillator : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
   {
+    // set movement factor
+    float cycles = Time.time / period; // grows continually from 0
+
+    const float tau = Mathf.PI * 2f; // about 6.28
+    float rawSinWave = Mathf.Sin(cycles * tau); // goes from -1 to +1
+
+    movementFactor = rawSinWave / 2f + 0.5f;
     Vector3 offset = movementVector * movementFactor;
     transform.position = startingPos + offset;
 	}
